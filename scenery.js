09d0d3c96f4d5dc1f5e69ed447e02ae478fb95ee
samairasingh565 <84003619+samairasingh565@users.noreply.github.com>
2023-03-25 23:15:04 +0530

    Add files via upload

---
+++ scenery.js
@@ -0,0 +1,42 @@
+img = "";
+status = "";
+
+function setup() {
+canvas = createCanvas(610,320);
+canvas.position(300,220);
+objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+document.getElementById("status").innerHTML = "Status : Detecting Object";
+
+
+}
+
+
+
+function draw(){
+image(img, 0,0,610,320);
+    
+}
+
+function modelLoaded(){
+
+    console.log("Model Loaded!")
+    status = true;
+    objectDetector.detect(img, gotResult);
+}
+    
+function gotResult(error, results) {
+
+if (error) {
+    console.log(error);
+}
+ 
+console.log(results);
+}
+
+function preload(){
+ 
+img = loadImage("9.png");
+
+}
+ 
+
